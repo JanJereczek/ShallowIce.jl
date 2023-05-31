a5c99000555a3e5f6c8f9239c3f0a6e73f888c97
“M-Lauritzen” <mikkel.lauritzen@nbi.ku.dk>
2023-05-31 16:45:51 +0200

    Debugged

--- src/utils2.jl
+++ src/utils2.jl
@@ -13,7 +13,21 @@ function gaussian_filter(x::Vector, y::Vector)
     for i in eachindex(x)
         yfilt[i] = sum(y .* normed_gaussian(x, x[i], sigma) )
     end
-    return yfilt[length(y)+1:end]
+    return yfilt[length(y)÷2+1:end]
+end
+
+function gaussian_filter_old(x::Vector, y::Vector)
+    
+    if length(x) != length(y)
+        error("Vector lengths do not match.")
+    end
+
+    yfilt = similar(y)
+    sigma = 2e3
+    for i in eachindex(x)
+        yfilt[i] = sum(y .* normed_gaussian(x, x[i], sigma) )
+    end
+    return yfilt
 end
 
 function normed_gaussian(x::Vector, mu::Real, sigma::Real)
